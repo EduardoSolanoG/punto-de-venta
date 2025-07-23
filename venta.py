e9ae8e65211d5d51f39426e0a372f3e0be973fef
Eduardo <lalogaming24@gmail.com>
2025-07-23 15:52:33 -0600

    proyecto final

--- venta.py
+++ venta.py
@@ -15,3 +15,23 @@ class venta:
                 print(f"Tienes {cantidad_actual} piezas de {producto.get_nombre()} en el carrito.")
                 opcion = input("¿Cuántas piezas deseas eliminar? (o escribe 'todo'): ")
 
+                if opcion.lower() == 'todo':
+                   producto.aumentar_stock(cantidad_actual)
+                   self.__productos.pop(indice)
+                   print("Prodcuto eliminado completamente")
+                else:
+                    cantidad = int(opcion)
+                    if 0 < cantidad < cantidad_actual:
+                    self.__productos[indice] = (producto, cantidad_actual - cantidad)
+                    producto.aunmentar_stock(cantidad)
+                    print(f"Se eliminaron {cantidad} piezas.")
+                elif cantidad == cantidad_actual:
+                    producto.aunmentar_stock(cantidad)
+                    self.__productos.pop(indice)
+                    print("Producto eliminado completamente")
+                else:
+                print("cantidad invalida.")
+            else:
+                print("Indice fuera de rango.")
+        except ValueError:
+            print("Debes ingresar un número o palabra 'todo'.")
